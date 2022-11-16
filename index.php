8dd7d3a6ce5db270e98c807db9a51f2eeddf5049
Lucas Senra <lucas.senra3@gmail.com>
2022-11-16 15:51:10 -0300

    Template das categorias

--- index.php
+++ index.php
@@ -315,5 +315,20 @@ $app->post("/admin/categories/:idcategory/", function($idcategory) {
 
 });
 
+$app->get("/categories/:idcategory/", function($idcategory){
+
+   $category = new Category();
+
+   $category->get((int)$idcategory);
+
+   $page = new Page();
+
+   $page->setTpl("category", [
+       'category'=>$category->getValues(),
+       'products'=>[]
+   ]);
+
+});
+
 $app->run(); //faz funcionar após tudo carregado
 
--- vendor/hcodebr/php-classes/src/Model/Category.php
+++ vendor/hcodebr/php-classes/src/Model/Category.php
@@ -26,6 +26,8 @@ class Category extends Model {
 
         $this->setData($results[0]);
 
+        Category::updateFile();
+
     }
 
     public function get($idcategory) {
@@ -48,5 +50,22 @@ class Category extends Model {
             ':idcategory'=>$this->getidcategory()
         ]);
 
+        Category::updateFile();
+
     }
+
+    public static function updateFile() {
+
+        $categories = Category::listAll();
+
+        $html = [];
+
+        foreach ($categories as $row) {
+            array_push($html, '<li><a href="/categories/'.$row['idcategory'].'">'.$row['descategory'].'</a></li>');
+        }
+
+        file_put_contents($_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . "views" . DIRECTORY_SEPARATOR . "categories-menu.html", implode('', $html)); //implode quando se quer um array para string, explode o contrário
+
+    }
+
 }
